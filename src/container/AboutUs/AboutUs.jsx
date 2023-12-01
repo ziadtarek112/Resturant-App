09f10b9ccffbc94e9a68822d3573ebbddc1ad651
ziad tarek <66745488+ziadtarek112@users.noreply.github.com>
2023-12-01 20:46:33 +0200

    About us Implemented

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
-
-import './AboutUs.css';
+import React from "react";
+import { images } from "../../constants";
+import "./AboutUs.css";
 
 const AboutUs = () => (
-  <div>
-    AboutUs
+  <div
+    className="app__aboutus app__bg flex__center section__padding"
+    id="#about"
+  >
+    <div className="app__aboutus-overlay flex__center">
+      <img src={images.G} alt="" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">About Us</h1>
+        <img src={images.spoon} alt="" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quia
+          incidunt officiis repudiandae culpa illum. Quos, dolor hic! Ipsam nam
+          vel id blanditiis dicta error hic aperiam nostrum quisquam est?
+        </p>
+        <button type="button" className="custom__button">Know more</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt="" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quia
+          incidunt officiis repudiandae culpa illum. Quos, dolor hic! Ipsam nam
+          vel id blanditiis dicta error hic aperiam nostrum quisquam est?
+        </p>
+        <button type="button" className="custom__button">Know more</button>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-
-import './Header.css';
+import React from "react";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div className="">
-    Header
+  <div className="app__header app__wrapper section__padding" id="#header">
+    <div className="app__wrapper_info">
+      <SubHeading title="Choose  the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+        voluptates molestias adipisci perferendis facilis quisquam rem,
+        blanditiis pariatur error unde tempore excepturi maiores commodi
+        necessitatibus, voluptatum maxime, deleniti quasi illo.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="" />
+    </div>
   </div>
 );
 
